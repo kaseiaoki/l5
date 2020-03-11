f99055cee9f89696c6a2b303df371884722ad752
aucuba <aoki.11235813@ezweb.ne.jp>
2020-03-11 13:56:16 +0900

    init

---
+++ l5.cpp
@@ -0,0 +1,13 @@
+#include <filesystem>
+#include <iostream>
+
+int main() {
+	std::filesystem::path path = std::filesystem::current_path();
+    for(const std::filesystem::directory_entry &i:std::filesystem::directory_iterator(".")){
+        if (std::filesystem::is_regular_file(i.path())) {
+            std::cout << "file: " << i.path().filename().string() << std::endl;
+        }else if (std::filesystem::is_directory(i.path())){
+            std::cout << "dir: " << i.path().filename().string() << std::endl;
+        }
+    }
+}
